2aee57c7b4313c5205493d99abe58939466e3a65
rohithhshiva786 <96860873+rohithhshiva786@users.noreply.github.com>
2023-03-08 12:48:31 +0530

    Delete src directory

--- src/main/java/com/example/demo/repo/Bloodrepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.reactsp.blooddona;
-
-public interface Bloodrepo extends JpaRepository<blooddona,Integer>{
-	
-}
